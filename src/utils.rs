15271267c7e94efa364d3c3835706fcb43c5e7e4
Lim Chun Leng <limchunleng@gmail.com>
2026-04-12 00:43:07 +0900

    fix: message longer than window breaks when using nvim_oxi::api::notify function

--- src/utils.rs
+++ src/utils.rs
@@ -1,18 +1,33 @@
-use nvim_oxi::{
-    Dictionary,
-    api::{self, types::LogLevel},
-};
+use nvim_oxi::api::{self, types::LogLevel};
 
-/// A wrapper for [nvim_oxi::api::notify]
+/// A wrapper for vim.notify that properly handles long lines and multiline messages
 ///
-/// This wrapper is created as [nvim_oxi::api::notify] has problem when being passed multi line str
+/// This uses Lua's vim.notify which:
+/// - Respects user's notification manager (nvim-notify, noice.nvim, etc.)
+/// - Properly handles long lines and multiline messages
+/// - Supports log levels with appropriate highlighting
 pub fn notify(message: impl ToString, log_level: LogLevel) {
-    let lines = message
-        .to_string()
-        .lines()
-        .map(|x| x.to_string())
-        .collect::<Vec<String>>();
-    for line in lines {
-        let _ = api::notify(&line, log_level, &Dictionary::new());
-    }
+    let msg = message.to_string();
+
+    // Map nvim-oxi LogLevel to Lua vim.log.levels
+    let lua_level = match log_level {
+        LogLevel::Error => "vim.log.levels.ERROR",
+        LogLevel::Warn => "vim.log.levels.WARN",
+        LogLevel::Info => "vim.log.levels.INFO",
+        LogLevel::Debug => "vim.log.levels.DEBUG",
+        _ => "vim.log.levels.INFO", // Default to INFO for any other log levels
+    };
+
+    // Escape the message for Lua string literal
+    let escaped = msg
+        .replace('\\', "\\\\")
+        .replace('\n', "\\n")
+        .replace('\r', "\\r")
+        .replace('\t', "\\t")
+        .replace('"', "\\\"");
+
+    let lua_code = format!("lua vim.notify(\"{}\", {})", escaped, lua_level);
+
+    // Execute using command
+    let _ = api::command(&lua_code);
 }
