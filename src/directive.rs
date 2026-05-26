b73bb4c507af5e62fd0287335bd96a06199485cd
Lim Chun Leng <limchunleng@gmail.com>
2026-05-26 09:52:23 +0900

    refactor: remove unused coding_basics.md

--- src/directive.rs
+++ src/directive.rs
@@ -215,16 +215,6 @@ pub fn load_system_directives() -> HashMap<String, Directive> {
     );
 
     map.insert(
-        "Coding Basics".into(),
-        Directive {
-            condition: None,
-            source: DirectiveSource::File {
-                paths: vec![directive_path("coding_basics.md")],
-            },
-        },
-    );
-
-    map.insert(
         "Code Comment Basics".into(),
         Directive {
             condition: None,
