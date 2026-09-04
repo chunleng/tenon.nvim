bf4b60f90b7cf856f99fd3506304125a00003238
Lim Chun Leng <limchunleng@gmail.com>
2026-09-04 17:17:33 +0900

    refactor: remove situation_sensitivity directive

--- src/directive.rs
+++ src/directive.rs
@@ -146,17 +146,6 @@ pub fn load_system_directives() -> HashMap<String, Directive> {
     );
 
     map.insert(
-        "Situation Sensitivity".into(),
-        Directive {
-            condition: None,
-            source: DirectiveSource::Preset {
-                id: "Situation Sensitivity".into(),
-                path: directive_path("situation_sensitivity.md"),
-            },
-        },
-    );
-
-    map.insert(
         "Read First Attitude".into(),
         Directive {
             condition: None,
